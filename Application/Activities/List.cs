a7b8df5e42cb9f9b9e619b02328d2bc3473928ae
Ufuk Inam <ufukinam@gmail.com>
2022-08-26 10:51:37 +0300

    end of section 4

--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -1,6 +1,7 @@
 using Domain;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using Persistence;
 
 namespace Application.Activities
@@ -19,6 +20,19 @@ namespace Application.Activities
 
             public async Task<List<Activity>> Handle(Query request, CancellationToken cancellationToken)
             {
+                //CancellationToken usage
+                /* try{
+                    for (var i = 0; i < 10; i++)
+                    {
+                        cancellationToken.ThrowIfCancellationRequested();
+                        await Task.Delay(1000, cancellationToken);
+                        _logger.LogInformation($"Task {i} has completed");
+                    }
+                } catch (Exception ex)
+                {
+                    _logger.LogInformation("Task was cancelled");
+                } 
+                return await _context.Activities.ToListAsync(cancellationToken);*/
                 return await _context.Activities.ToListAsync();
             }
         }
