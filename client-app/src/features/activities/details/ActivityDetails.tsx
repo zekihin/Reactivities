aa8baa19930ddf2c366b66c2351b0ccdf84fa983
Ufuk nam <ufukinam@gmail.com>
2022-08-30 16:59:10 +0300

    End of section 5

--- client-app/src/features/activities/details/ActivityDetails.tsx
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -1,13 +1,14 @@
 import React from "react";
-import { Button, Card, Icon, Image } from "semantic-ui-react";
+import { Button, Card, Image } from "semantic-ui-react";
 import { Activity } from "../../../app/models/activity";
 
 interface Props {
     activity: Activity;
     cancelSelectActivity: () => void;
+    openForm: (id: string) => void;
 }
 
-export default function ActivityDetails({activity, cancelSelectActivity}: Props) {
+export default function ActivityDetails({activity, cancelSelectActivity, openForm}: Props) {
     return (
         <Card fluid>
             <Image src={`/assets/categoryImages/${activity.category}.jpg`} />
@@ -22,7 +23,7 @@ export default function ActivityDetails({activity, cancelSelectActivity}: Props)
             </Card.Content>
             <Card.Content extra>
                 <Button.Group withs='2'>
-                    <Button basic color='blue' content='Edit' />
+                    <Button onClick={() => openForm(activity.id)} basic color='blue' content='Edit' />
                     <Button onClick={cancelSelectActivity} basic color='grey' content='Cancel' />
                 </Button.Group>
             </Card.Content>
