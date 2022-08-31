89d4cefd28afdb528a8b5df212f29b8a1d799c84
Ufuk Inam <ufukinam@gmail.com>
2022-08-31 17:47:06 +0300

    Mid of section 8

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,16 +1,21 @@
 import { observer } from "mobx-react-lite";
-import React from "react";
+import React, { useEffect } from "react";
 import { Grid } from "semantic-ui-react";
+import LoadingComponent from "../../../app/layout/LoadingComponent";
 import { useStore } from "../../../app/stores/store";
-import ActivityDetails from "../details/ActivityDetails";
-import ActivityForm from "../form/ActivityForm";
 import ActivityList from "./ActivityList";
 
 export default observer (function ActivityDashboard() {
 
     const {activityStore} = useStore();
 
-    const {selectedActivity, editMode} = activityStore;
+    useEffect(() => {
+      activityStore.loadActivities();
+    }, [activityStore])
+  
+  
+  
+    if(activityStore.loadingInitial) return <LoadingComponent content='Loading app' />
 
     return (
         <Grid>
@@ -18,10 +23,7 @@ export default observer (function ActivityDashboard() {
             <ActivityList />
             </Grid.Column>
             <Grid.Column width='6'>
-                {selectedActivity && !editMode &&
-                <ActivityDetails />}
-                {editMode &&    
-                <ActivityForm />}
+                <h2>Activity Filters</h2>
             </Grid.Column>
         </Grid>
     )
