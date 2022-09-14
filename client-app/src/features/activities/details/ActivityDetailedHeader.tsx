c057ec106c3b15b1f152f855c2f4f47f249a9ce7
Ufuk Inam <ufukinam@gmail.com>
2022-09-14 17:40:24 +0300

    Mid of section 15

--- client-app/src/features/activities/details/ActivityDetailedHeader.tsx
+++ client-app/src/features/activities/details/ActivityDetailedHeader.tsx
@@ -1,8 +1,9 @@
 import { format } from 'date-fns';
 import { observer } from 'mobx-react-lite';
 import { Link } from 'react-router-dom';
-import {Button, Header, Item, Segment, Image} from 'semantic-ui-react'
-import {Activity} from "../../../app/models/activity";
+import { Button, Header, Item, Segment, Image } from 'semantic-ui-react'
+import { Activity } from "../../../app/models/activity";
+import { useStore } from '../../../app/stores/store';
 
 const activityImageStyle = {
     filter: 'brightness(30%)'
@@ -21,11 +22,12 @@ interface Props {
     activity: Activity
 }
 
-export default observer (function ActivityDetailedHeader({activity}: Props) {
+export default observer(function ActivityDetailedHeader({ activity }: Props) {
+    const {activityStore: {updateAttendence, loading}} = useStore();
     return (
         <Segment.Group>
-            <Segment basic attached='top' style={{padding: '0'}}>
-                <Image src={`/assets/categoryImages/${activity.category}.jpg`} fluid style={activityImageStyle}/>
+            <Segment basic attached='top' style={{ padding: '0' }}>
+                <Image src={`/assets/categoryImages/${activity.category}.jpg`} fluid style={activityImageStyle} />
                 <Segment style={activityImageTextStyle} basic>
                     <Item.Group>
                         <Item>
@@ -33,11 +35,11 @@ export default observer (function ActivityDetailedHeader({activity}: Props) {
                                 <Header
                                     size='huge'
                                     content={activity.title}
-                                    style={{color: 'white'}}
+                                    style={{ color: 'white' }}
                                 />
                                 <p>{format(activity.date!, 'dd MMM yyyy')}</p>
                                 <p>
-                                    Hosted by <strong>Bob</strong>
+                                    Hosted by <strong><Link to={`/profiles/${activity.host?.username}`}>{activity.host?.displayName}</Link></strong>
                                 </p>
                             </Item.Content>
                         </Item>
@@ -45,11 +47,16 @@ export default observer (function ActivityDetailedHeader({activity}: Props) {
                 </Segment>
             </Segment>
             <Segment clearing attached='bottom'>
-                <Button color='teal'>Join Activity</Button>
-                <Button>Cancel attendance</Button>
-                <Button as={Link} to={`/manage/${activity.id}`} color='orange' floated='right'>
-                    Manage Event
-                </Button>
+                {activity.isHost ? (
+                    <Button as={Link} to={`/manage/${activity.id}`} color='orange' floated='right'>
+                        Manage Event
+                    </Button>
+                ) : activity.isGoing ? (
+                    <Button loading={loading} onClick={updateAttendence}>Cancel attendance</Button>
+                ) : (
+                    <Button loading={loading} onClick={updateAttendence} color='teal'>Join Activity</Button>
+                )}
+
             </Segment>
         </Segment.Group>
     )
